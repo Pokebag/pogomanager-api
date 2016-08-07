0b3b1a7d4f1bfab3bf4070b7b0d75b7b293afa52
tpeebles <trezy.peebles@gettyimages.com>
2016-08-07 12:03:37 -0500

    Giant refactor
    * Middleware and routes are now set up in separate files
    * login and getItemTemplates middlewares now only run on the routes that need them
    * MongoDB is now a requirement for storing users on the backend
    * auth doesn't actually work yet but it's super close

--- middleware/login.js
+++ middleware/login.js
@@ -7,11 +7,16 @@ let pogobuf = require('pogobuf')
 
 
 function * login (next) {
-  let login = new pogobuf.GoogleLogin()
+  let GoogleAuth = new pogobuf.GoogleLogin()
 
   this.state.client = new pogobuf.Client()
 
-  let token = yield login.login(this.state.username, this.state.password)
+  let token = yield GoogleAuth.getToken(this.session.email, this.session.token)
+  .then(authData => {
+    return authData.Auth
+  })
+
+  console.log(token)
 
   this.state.client.setAuthInfo('google', token)
 
