6fb808b4e3b7489ad2905916d46365938ce3f0c2
tpeebles <trezy.peebles@gettyimages.com>
2016-08-07 11:11:15 -0500

    Add a basic model for storing a user's master token in the database

---
+++ models/User.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let bcrypt = require('bcrypt')
+let co = require('co')
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+
+
+
+
+
+let UserSchema = new Schema({
+  email: {
+    required: true,
+    type: String,
+    unique: true
+  },
+  token: {
+    required: true,
+    type: String
+  }
+})
+
+
+
+
+
+module.exports = mongoose.model('User', UserSchema)
